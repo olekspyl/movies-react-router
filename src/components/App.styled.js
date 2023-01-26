62247062e862fdda532996116f6a293e16e18840
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-01-26 22:50:54 +0100

    add style and image

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -39,6 +39,7 @@ export const Movie = styled.li`
 display: flex;
 text-decoration: none;
 margin-bottom: 10px;
+font-size: 20px;
 `
 
 export const MovieLink = styled(NavLink)`
@@ -53,5 +54,77 @@ color: black;
 :hover:not(.active),
 :focus-visible:not(.active) {
     color: grey;
+}
+`
+
+export const MovieContainer = styled.div`
+padding: 20px;
+display: flex;
+`
+
+export const MoviePoster = styled.img`
+margin-right: 20px;
+`
+
+
+
+export const Title = styled.h1`
+margin: 0 0 20px 0;
+`
+export const Score = styled.p`
+font-size: 20px;
+margin: 0 0 20px 0;
+`
+export const OverviewTitle = styled.h3`
+margin: 0 0 5px 0;
+`
+export const Overview = styled.p`
+margin: 0 0 20px 0;
+`
+
+export const GenresTitle = styled.h4`
+margin: 0 0 5px 0;
+
+`
+
+export const GenresList = styled.ul`
+margin: 0 0 20px 0;
+list-style: none;
+display: flex;
+padding: 0px;
+
+`
+export const GenresItem = styled.li`
+padding-right: 15px;
+`
+
+export const AdditionalInfo = styled.h2`
+margin: 0 0 20px 0;
+`
+
+export const CastLink = styled(NavLink)`
+text-decoration: none;
+color: black;
+font-size: 20px;
+font-weight: medium;
+margin-right: 10px;
+
+
+:hover:not(.active),
+:focus-visible:not(.active) {
+    color: #a35c74;
+}
+`
+
+export const ReviewsLink = styled(NavLink)`
+text-decoration: none;
+color: black;
+font-size: 20px;
+font-weight: medium;
+
+
+:hover:not(.active),
+:focus-visible:not(.active) {
+    color: #a35c74;
 }
 `
--- src/components/pages/MovieDetails.js
+++ src/components/pages/MovieDetails.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from "react";
-import { useParams, NavLink } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import { MovieContainer, MoviePoster, Score, Title, OverviewTitle, Overview, GenresTitle, GenresList, AdditionalInfo, GenresItem, CastLink, ReviewsLink } from "components/App.styled";
 
 
 
 const MovieDetails = () => {
     const [movie, setMovie] = useState(null);
     const { movieId } = useParams();
-
-
     
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=542013762b3531b404b15130444dfd0b`).then(
@@ -16,25 +15,42 @@ const MovieDetails = () => {
         ).catch(error => console.log(error));
     }, [movieId]);
 
-    if (!movie) {
+    
+ if (!movie) {
         return;
     }
-    const {title, release_date, vote_average, overview, genres} = movie;
-    const allGenres = genres.map(genre => genre.name+" ");
 
+ const { title, release_date, vote_average, overview, genres, poster_path } = movie;
+
+    const getPosterURL = (posterPath) => {
+        return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2${posterPath}`
+    }
+   
+    
+    const allGenres = genres.map(genre =>
+        <GenresItem key={genre.id}>{genre.name}</GenresItem>);
+    
+    const date = new Date(release_date);
     
     return (
-        <div>
-            {/* <img src={movie.belongs_to_collection.poster_path} alt={movie.belongs_to_collection.backdrop_path}/> */}
-            <h1>{title }</h1>
-            <p>{release_date}</p>
-            <p>{Number.parseInt(vote_average * 100 / 10)}%</p>
-            <p>{overview }</p>
-            <p>{allGenres}</p>
-            <h2>Additional information</h2>
-            {/* <NavLink to={`${movieId/cast}`}></NavLink> */}
-            <NavLink></NavLink>
-        </div>
+        <MovieContainer>
+            <div>
+            <MoviePoster loading="lazy" src={getPosterURL(poster_path)} alt={title} />
+            </div>
+            <div>
+            <Title>{title } {date.getFullYear()}</Title>
+            <Score>User score {Number.parseInt(vote_average * 100 / 10)}%</Score>
+                <OverviewTitle>Overview</OverviewTitle>
+                <Overview>{overview}</Overview>
+                <GenresTitle>Genres</GenresTitle>
+                <GenresList>{allGenres}</GenresList>
+            <AdditionalInfo>Additional information</AdditionalInfo>
+                <CastLink>Cast</CastLink>
+                <ReviewsLink>Reviews</ReviewsLink>
+
+                
+                    </div>
+        </MovieContainer>
     )
 };
 
