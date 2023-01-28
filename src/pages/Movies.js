9c4c72cec2c8b82894692b1997bc5b99810915cb
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-01-28 19:01:51 +0100

    without back button

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,43 +1,49 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { FormContainer, Button, Input } from "components/App.styled";
-
-
 
 const Movies = () => {
     const [query, setQuery] = useState('');
     const [submit, setSubmit] = useState(false);
     const [movies, setMovies] = useState([]);
 
+     const { movieId } = useParams();
+
     useEffect(() => {
         if (submit) { 
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=542013762b3531b404b15130444dfd0b&query=${query}`).then(
-            response => setMovies(response.data)
+            response => setMovies(response.data.results)
         ).catch(error => console.log(error));
         } 
+        setSubmit(false);
         
-        
-    }, [query, submit, movies]);
+    }, [query, submit]);
+    
+
 
 
     const onQueryChange = e => {
         setQuery(e.target.value);
     }
 
+    const onHandleClick = () => {
+        setSubmit(true);
+    }
 
 
     const onSubmit = e => {
         e.preventDefault();
-        setSubmit(true);
+        onHandleClick();
         console.log(movies)
     }
 
 
-
+    if (!movieId) {
     return (
+        
     <FormContainer>
-        <form >
+        <form onSubmit={onSubmit}>
             <label>
                     <Input type="text"
                         value={query}
@@ -45,10 +51,14 @@ const Movies = () => {
                 </Input>
             </label>
         </form>
-        <Button type="button" onClick={onSubmit}>Search</Button>
-        <Outlet /> 
+        <Button type="button" onClick={onHandleClick}>Search</Button>
+         
     </FormContainer>
     )
+    } else {
+        return <Outlet />
+}
+    
 }
 
 export default Movies;
