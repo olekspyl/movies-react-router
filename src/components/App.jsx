f70da73f118145b3fe57d57468176d58d0acde59
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-08-05 23:49:15 +0200

    white sheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import { lazy, Suspense } from "react";
-import Header from './Header';
-const Home = lazy(() => import('../pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('../pages/MovieDetails'));
-const Cast = lazy(() => import('../pages/Cast'));
-const Reviews = lazy(() => import('../pages/Reviews'));
 
 
-
-
-const App = () => {
+export const App = () => {
   return (
     <div>
-      <Header />
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/movies' element={<Movies/>}/>
-          <Route path='/movies/:movieId' element={<MovieDetails />}>
-            <Route path='/movies/:movieId/cast' element={<Cast/>}/>
-            <Route path='/movies/:movieId/reviews' element={<Reviews/>}/>
-          </Route>
-            <Route path="*" element={<Home />} />
-        </Routes>
-        </Suspense>
        </div>
   );
 };
 
-export default App;
 
