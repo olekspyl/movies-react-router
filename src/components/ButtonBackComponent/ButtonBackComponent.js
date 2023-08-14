2fe498d0472b0cc66ddd845c9b7249d2bb00b733
Oleksandra Pylypchuk <99043767+olekspyl@users.noreply.github.com>
2023-08-14 16:24:05 +0200

    Update ButtonBackComponent.js

--- src/components/ButtonBackComponent/ButtonBackComponent.js
+++ src/components/ButtonBackComponent/ButtonBackComponent.js
@@ -1,7 +1,7 @@
 import { ButtonBack, LinkBack } from "components/App.styled";
 
 const ButtonBackComponent = ({ location }) => {
-       const backLinkHref = location.state?.from ?? "/";
+       const backLinkHref = location.state.from ?? "/";
     return (
     <ButtonBack type="button">
         <LinkBack state={{ from: backLinkHref }} to={"/"}>Back</LinkBack>
