3fdb315c9d56b044a032ebb8061bbc46cbf317a8
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-08-28 13:30:35 +0200

    fix btn

--- src/components/ButtonBackComponent/ButtonBackComponent.js
+++ src/components/ButtonBackComponent/ButtonBackComponent.js
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom';
 import { Button } from './Button.styled';
 
 const ButtonBackComponent = ({ location }) => {
-  //   const location = useLocation();
-
   return (
     <Button type="button" className="btn btn-outline-dark">
       <Link to={location}>Back</Link>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,36 +1,35 @@
-import { useState, useEffect } from "react";
-import { useParams, Outlet, useLocation } from "react-router-dom";
-import axios from "axios";
-import MovieComponent from "components/MovieComponent";
-import ButtonBackComponent from "components/ButtonBackComponent";
-
+import { useState, useEffect, useRef } from 'react';
+import { useParams, Outlet, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import MovieComponent from 'components/MovieComponent';
+import ButtonBackComponent from 'components/ButtonBackComponent';
 
 const MovieDetails = () => {
-    const [movie, setMovie] = useState(null);
-    const { movieId } = useParams();
-    const location = useLocation();
- 
-    
-    useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=022b72fb5c3086143a7c2d5ac5a186e1`).then(
-            response => setMovie(response.data)
-        ).catch(error => console.log(error));
-    }, [movieId]);
+  const [movie, setMovie] = useState(null);
+  const { movieId } = useParams();
+  const location = useLocation();
+  const back = useRef(location.state?.from ?? '/movies');
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=022b72fb5c3086143a7c2d5ac5a186e1`
+      )
+      .then(response => setMovie(response.data))
+      .catch(error => console.log(error));
+  }, [movieId]);
 
-    if (!movie) {
-        return;
-    };
-    
+  if (!movie) {
+    return;
+  }
 
-
-    return (      
-        <>
-        <ButtonBackComponent location={location } />
-            <MovieComponent movie={movie} />
-        <Outlet/>
-     </>
-    );
+  return (
+    <>
+      <ButtonBackComponent location={back.current} />
+      <MovieComponent movie={movie} />
+      <Outlet />
+    </>
+  );
 };
 
 export default MovieDetails;
